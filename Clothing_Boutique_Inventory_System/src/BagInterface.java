6ff150fa8f1e8c3f65fb8cabfee57ba1068d8902
micahcourey <micahjcourey@gmail.com>
2017-03-25 23:47:19 -0700

    Add initial Java class files for Clothing Boutique program

---
+++ Clothing_Boutique_Inventory_System/src/BagInterface.java
@@ -0,0 +1,12 @@
+/**
+ Name: Micah Courey
+ Project Name: CSC400-CTA2
+ Project Purpose: A Clothing Boutique Inventory System
+ Algorithm Used: The Bag ADT is implemented in this project
+ Program Inputs:
+ Program Outputs:
+ Program Limitations:
+ Program Errors:
+ */
+public class BagInterface {
+}
